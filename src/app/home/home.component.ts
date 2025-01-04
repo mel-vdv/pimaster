256cbf5923e1785a2d46c782fd0a0f9554a9638e
Mélanie Vandervaeren <melvdv@yahoo.fr>
2025-01-04 18:21:16 +0100

    six

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,5 +13,5 @@ export class HomeComponent implements OnInit {
   }
 
   bevetLink = "https://bevet.be/clinique/cabinet-veterinaire-wanze/";
-  mapsLink = "https://www.google.fr/maps/place/Rue+du+Chaufour+18,+5190+Jemeppe-sur-Sambre/@50.4872172,4.6668631,14.27z/data=!4m6!3m5!1s0x47c18462844185e9:0x21861a12aef335af!8m2!3d50.4892017!4d4.6993424!16s%2Fg%2F11cpk8f0h7?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D";
+  mapsLink = "https://www.google.be/maps/place/Cabinet+v%C3%A9t%C3%A9rinaire+BeVet+-+Wanze+(Urgences+24h%2F24)/@50.5375227,5.1974351,17z/data=!3m1!4b1!4m6!3m5!1s0x47c1a9b15a958bc5:0x100779d454fffa09!8m2!3d50.5375193!4d5.20001!16s%2Fg%2F1ptwnsrb9?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D";
 }
